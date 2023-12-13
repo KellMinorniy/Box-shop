3d95a5bf5c8b5b3f4b675a0c8419e02fc887ed21
Solo Coder <kisliydanecka@gmail.com>
2023-12-13 23:38:58 +0300

    template header

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <script setup>
+import HeaderShop from "./components/HeaderShop.vue"
+
 </script>
 
 <template>
+  <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
+    <HeaderShop />
+  </div>
 </template>
 
 <style scoped>
