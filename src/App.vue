872ac09f3395f8359ed34313a8e21667cc46738e
Solo Coder <kisliydanecka@gmail.com>
2023-12-14 06:45:19 +0300

    template sorted and filter

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,28 @@ import Drawer from "./components/DrawerShop.vue";
 
 <template>
   <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14">
-    <Drawer />
+    <!-- <Drawer /> -->
     <HeaderShop />
 
     <div class="p-10">
-      <h2 class="text-3xl font-bold text-slate-700 mb-8">Все товары:</h2>
+      <div class="flex justify-between items-center">
+        <h2 class="text-3xl font-bold text-slate-700 mb-8">Все товары:</h2>
+
+        <div class="flex gap-4">
+          <select name="" id="" class="py-2 px-3 border rounded-md outline-none">
+            <option value="">По названию</option>
+            <option value="">Сначала дешевые</option>
+            <option value="">Сначала дорогие</option>
+            <option value="">Все</option>
+          </select>
+  
+          <div class="relative">
+            <img src="/search.svg" alt="" class="absolute left-3 top-3">
+            <input type="text" placeholder="Поиск..." class="border rounded-md py-2 pl-11 pr-4 outline-none focus:border-gray-400">
+          </div>
+        </div>
+        </div>
+
 
       <CardList />
     </div>
