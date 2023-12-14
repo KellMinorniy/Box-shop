6d0970294a62234fcb2e58761bcaf8a618301af3
Solo Coder <kisliydanecka@gmail.com>
2023-12-14 16:41:18 +0300

    settings ref

--- src/components/HeaderShop.vue
+++ src/components/HeaderShop.vue
@@ -5,10 +5,10 @@
 <template>
     <header class="flex justify-between border-b border-slate-300 px-10 py-8">
       <div class="flex items-center gap-4">
-        <img src="/logo.png" alt="Logo" class="w-10">
+        <img src="/CBOX-Logo.jpg" alt="Logo" class="w-10">
         <div>
-          <h2 class="text-xl font-bold uppercase">Box-Shop</h2>
-          <p class="text-slate-400">Магазин чего - то</p>
+          <h2 class="text-xl font-bold uppercase">C-box</h2>
+          <p class="text-slate-400">Крымский маркетплейс</p>
         </div>
       </div>
         
